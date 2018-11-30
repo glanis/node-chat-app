1e6af1ff97fe039409a8d98f843f9784c42857c0
ShambhaviRB <shambhavibelligatti@gmail.com>
2018-11-30 18:07:26 +0530

    broadcast added

--- server/server.js
+++ server/server.js
@@ -14,6 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+
+    socket.emit('newMessage', {
+        from : "admin",
+        text : "welcome to chat app",
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from : "admin",
+        text : "New user joined",
+        createdAt: new Date().getTime()
+    });
+    
   socket.on('createMessage', (message) => {
       console.log("create message", message);
       io.emit('newMessage', {
